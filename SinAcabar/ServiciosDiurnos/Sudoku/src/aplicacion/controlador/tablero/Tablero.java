3f266cbfc7d65aa122182e540e193ee6e9c11bb6
Mario Codes <mariocodes1993@gmail.com>
2016-12-13 15:29:48 +0100

    Antes de intentar arreglar la chapuza de 'Facade.rellenoCuadradoGrafico()'.

--- SinAcabar/ServiciosDiurnos/Sudoku/src/aplicacion/controlador/tablero/Tablero.java
+++ SinAcabar/ServiciosDiurnos/Sudoku/src/aplicacion/controlador/tablero/Tablero.java
@@ -30,103 +30,29 @@ public class Tablero {
      */
     private void preparacionTablero() {
         iniElementosNecesarios();
-//        rellenoFilas();
-//        rellenoColumnas();
     }
     
     /**
-     * Inicializacion de los Cuadrados. 
-     * todo: Mirar de automatizarlo con un 'for' en vez de hacerlo a mano. no tengo ganas de comermer la cabeza ahora.
+     * Inicializacion de los Cuadrados, Filas y Columnas. 
      */
     private void iniElementosNecesarios() {
                                 //numCuadrado, numFila, numColumna.
-        CUADRADOS[0] = new Cuadrado(0, 0, 0);
-        CUADRADOS[1] = new Cuadrado(1, 0, 3);
-        CUADRADOS[2] = new Cuadrado(2, 0, 6);
-        CUADRADOS[3] = new Cuadrado(3, 3, 0);
-        CUADRADOS[4] = new Cuadrado(4, 3, 3);
-        CUADRADOS[5] = new Cuadrado(5, 3, 6);
-        CUADRADOS[6] = new Cuadrado(6, 6, 0);
-        CUADRADOS[7] = new Cuadrado(7, 6, 3);
-        CUADRADOS[8] = new Cuadrado(8, 6, 6);
-        
-        for (int i = 0; i < FILAS.length; i++) {
+        for (int i = 0, indiceFila = 0, indiceColumna = 0; i < CUADRADOS.length; i++) {
+            CUADRADOS[i] = new Cuadrado(i, indiceFila, indiceColumna);
             FILAS[i] = new Fila();
             COLUMNAS[i] = new Columna();
+            
+            if((i+1)%3 == 0) {
+                indiceFila += 3;
+                indiceColumna = 0;
+            } else {
+                indiceColumna += 3;
+            }
         }
     }
     
-//    /**
-//     * Introduccion en si misma. Se debera repetir 3 veces.
-//     * @param numPrimerCuadrado Numero del primer cuadrado. Debera ser 0/3/6.
-//     */
-//    private void introduccionCasillasCuadradosEnFilas(int numPrimerCuadrado) {
-//        Casilla[] casillasTmp; //Almacen temporal hasta que este la fila completa y se introduzcan.
-//        
-//        for (int indiceFila = numPrimerCuadrado, indiceCasillaTmp, indiceCasillaCuadrado = 0; indiceFila <= numPrimerCuadrado+2; indiceFila++, indiceCasillaCuadrado += 3) {
-//            casillasTmp = new Casilla[9];
-//            indiceCasillaTmp = 0;
-//            for (int x = 0; x <= 2; x++) {
-//                casillasTmp[indiceCasillaTmp++] = CUADRADOS[numPrimerCuadrado].getCASILLAS()[indiceCasillaCuadrado];
-//                casillasTmp[indiceCasillaTmp++] = CUADRADOS[numPrimerCuadrado+1].getCASILLAS()[indiceCasillaCuadrado+1];
-//                casillasTmp[indiceCasillaTmp++] = CUADRADOS[numPrimerCuadrado+2].getCASILLAS()[indiceCasillaCuadrado+2];
-//            }
-//            FILAS[indiceFila] = new Fila(casillasTmp);
-//        }
-//    }
-    
-//    /**
-//     * Introduccion de las Casillas de los Cuadrados en las Filas.
-//     */
-//    private void rellenoFilas() {
-//        introduccionCasillasCuadradosEnFilas(0);
-//        introduccionCasillasCuadradosEnFilas(3);
-//        introduccionCasillasCuadradosEnFilas(6);
-//    }
-    
-//    /**
-//     * Obtencion del numero de la primera columna que no se haya instanciado, para saber a partir de cual empezar a rellenar.
-//     * @return Int con la primera posicion a rellenar.
-//     */
-//    private int obtenerPrimeraColumnaNula() {
-//        for (int i = 0; i < COLUMNAS.length; i++) {
-//            if(COLUMNAS[i] == null) return i;
-//        }
-//        
-//        return -1; //Nunca deberia llegar a este punto.
-//    }
-    
-//    /**
-//     * Idem a las Filas pero introduciendo las Casillas en las columnas.
-//     * @param numPrimerCuadrado Numero del primer cuadrado. Debera ser 0/1/2.
-//     */
-//    private void introduccionCasillasCuadradosEnColumnas(int numPrimerCuadrado) {
-//        Casilla[] casillasTmp;
-//        int indicePrimeraColumna = obtenerPrimeraColumnaNula();
-//        
-//        for (int indiceColumna = indicePrimeraColumna, indiceCasillaTmp, indiceCasillaCuadrado = 0; indiceColumna <= indicePrimeraColumna+2; indiceColumna++, indiceCasillaCuadrado += 1) {
-//            casillasTmp = new Casilla[9];
-//            indiceCasillaTmp = 0;
-//            for (int i = 0; i <= 2; i++) {
-//                casillasTmp[indiceCasillaTmp++] = CUADRADOS[numPrimerCuadrado].getCASILLAS()[indiceCasillaCuadrado];
-//                casillasTmp[indiceCasillaTmp++] = CUADRADOS[numPrimerCuadrado+3].getCASILLAS()[indiceCasillaCuadrado+3];
-//                casillasTmp[indiceCasillaTmp++] = CUADRADOS[numPrimerCuadrado+6].getCASILLAS()[indiceCasillaCuadrado+6];
-//            }
-//            COLUMNAS[indiceColumna] = new Columna(casillasTmp);
-//        }
-//    }
-    
-//    /**
-//     * Introduccion de las Casillas en las columnas correspondientes.
-//     */
-//    private void rellenoColumnas() {
-//        introduccionCasillasCuadradosEnColumnas(0);
-//        introduccionCasillasCuadradosEnColumnas(1);
-//        introduccionCasillasCuadradosEnColumnas(2);
-//    }
-    
     /**
-     * Le asignamos a cada casilla su numero propio. Si se llega a un punto muerto, se recomienza.
+     * Le asignamos a cada casilla su numero propio. Si se llega a un punto muerto, se recomenzara con otro tablero.
      * @return Booleano para chequear cuando se ha generado correctamente.
      */
     public boolean asignacionNumeroCasillas() {
@@ -134,7 +60,7 @@ public class Tablero {
             Casilla[] casillasCuadrado = CUADRADOS[i].getCASILLAS();
             for (int x = 0; x < casillasCuadrado.length; x++) {
                 int num = GeneracionNumeros.generacionNumeroCasilla(this, casillasCuadrado[x]);
-                if(num == -1) return false;
+                if(num == -1) return false; //Punto muerto.
                 casillasCuadrado[x].setNumeroPropio(num);
             }
         }
