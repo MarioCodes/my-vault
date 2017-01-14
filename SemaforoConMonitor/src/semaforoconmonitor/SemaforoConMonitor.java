093d9ac3400fa8a9d6c096d9630ced79b25ce239
Mario Codes <mariocodes1993@gmail.com>
2017-01-14 14:38:35 +0100

    Arreglado los semaforos. Me habia olvidado de hacer un notifyAll() por lo que se quedaban siempre colgados en el wait().

--- SemaforoConMonitor/src/semaforoconmonitor/SemaforoConMonitor.java
+++ SemaforoConMonitor/src/semaforoconmonitor/SemaforoConMonitor.java
@@ -5,6 +5,9 @@
  */
 package semaforoconmonitor;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Replicacion del funcionamiento de un Semaforo mediante el uso de Monitores y metodos sincronizados.
  * @author Mario Codes Sánchez
@@ -16,7 +19,29 @@ public class SemaforoConMonitor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        try {
+            Semaforo s = new Semaforo(2);
+            
+            Runnable runnable1 = () -> s.adquirir();
+            Runnable runnable2 = () -> s.adquirir();
+            
+            Thread thread1 = new Thread(runnable1);
+            Thread thread2 = new Thread(runnable2);
+            
+            thread1.start();
+            thread2.start();
+            
+            thread1.join();
+            thread2.join();
+            
+            Thread thread3 = new Thread(runnable1);
+            thread3.start();
+            thread3.join();
+            
+            System.out.println(s.getPermisos());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SemaforoConMonitor.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
