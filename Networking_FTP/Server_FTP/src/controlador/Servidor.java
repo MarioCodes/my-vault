2379feca08f519fde5c336984d69604eddd7b0b3
Mario Codes <mariocodes1993@gmail.com>
2017-01-19 18:16:58 +0100

    Quitada la inner class del Server. Movida su funcionalidad a una Lambda

--- Networking_FTP/Server_FTP/src/controlador/Servidor.java
+++ Networking_FTP/Server_FTP/src/controlador/Servidor.java
@@ -22,73 +22,54 @@ public class Servidor {
     private static final int BUFFER_LENGTH = 16384; //Tamaño del buffer que se enviara de golpe.
     private static final int PUERTO = 8142;
     
+    private static Socket socket = null;    
+    private static InputStream in = null;
+    private static OutputStream out = null;
+        
+    /**
+     * Accion de recibir y guardar el fichero recibido.
+     * Se ejecuta en un hilo aparte.
+     */
+    private static void recibirFichero() {
+        try {
+            in = socket.getInputStream();
+            out = new FileOutputStream("ficheros/fichero.txt"); //todo: mas adelante debera ser variable. No hardcodeado.
+
+            byte[] bytes = new byte[BUFFER_LENGTH];
+
+            int count;
+            while((count = in.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
+            }
+        }catch(IOException ex) {
+            ex.printStackTrace();
+        }finally {
+            try {
+                if(out != null) out.close();
+                if(in != null) in.close();
+                if(socket != null) socket.close();
+            }catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
     /**
      * Puesta en marcha del Servidor. Se quedara a la espera de que haya conexiones entrantes.
      * Cada cliente tendra su propio hilo aparte.
      */
     public static void ejecucion() {
         try {
-            Socket socket = null;
             ServerSocket serverSocket = new ServerSocket(PUERTO); //Espera y escucha la llegada de los clientes. Una vez establecida, devuelve el Socket.; 
         
             while(true) {
                 socket = serverSocket.accept(); /* El ServerSocket me da el Socket.
                                                         Bloquea el programa en esta linea y solo avanza cuando un cliente se conecta.*/
 
-                new Tarea(socket).start(); //Cada Tarea en un nuevo Hilo.
+                new Thread(() -> recibirFichero()).start(); //Comienzo de la faena en un Hilo aparte.
             }
         }catch(IOException ex) {
             ex.printStackTrace();
-        }
-    }
-    
-    /**
-     * Inner Class encargada de la Tarea de atender a un Cliente.
-     */
-    static class Tarea extends Thread {
-        private Socket socket = null;
-        
-        private InputStream in = null;
-        private OutputStream out = null;
-        
-        /**
-         * Constructor a usar por defecto.
-         * @param socket Socket que estamos usando para este Hilo.
-         */
-        public Tarea(Socket socket) {
-            this.socket = socket;
-        }
-        
-        /**
-         * Accion de recibir y guardar el fichero recibido.
-         */
-        private void recibirFichero() {
-            try {
-                in = socket.getInputStream();
-                out = new FileOutputStream("ficheros/fichero.txt"); //todo: mas adelante debera ser variable. No hardcodeado.
-                
-                byte[] bytes = new byte[BUFFER_LENGTH];
-
-                int count;
-                while((count = in.read(bytes)) > 0) {
-                    out.write(bytes, 0, count);
-                }
-            }catch(IOException ex) {
-                ex.printStackTrace();
-            }finally {
-                try {
-                    if(out != null) out.close();
-                    if(in != null) in.close();
-                    if(socket != null) socket.close();
-                }catch(IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-        
-        @Override
-        public void run() {
-            recibirFichero();
         }
     }
 }
