d999b6c00979b4b2d01fa7d7895dee381215a579
Mario Codes <mariocodes1993@gmail.com>
2017-01-21 16:40:25 +0100

    Completado el Trigger para Insertar. Falta Delete y Update

--- Proyecto_Hibernate/desarrollando_vista.sql
+++ Proyecto_Hibernate/desarrollando_vista.sql
@@ -1,22 +1,26 @@
-SELECT *
-  FROM VISTA_ACTIVIDADES_ALOJAMIENTO;
-
+DELETE FROM alojamiento
+  WHERE id_alojamiento > 5;
+DELETE FROM actividad
+  WHERE id_actividad > 5;
+DELETE FROM REALIZA
+  WHERE alojamiento_id_alojamiento > 5 AND actividad_id_actividad > 5;
+  
 SELECT *
   FROM ALOJAMIENTO;
   
-DELETE FROM alojamiento
-  WHERE id_alojamiento > 5;
+SELECT *
+  FROM REALIZA;
   
 SELECT *
   FROM ACTIVIDAD;
 
-DELETE FROM actividad
-  WHERE id_actividad > 5;
+SELECT *
+  FROM vista_actividades_alojamiento;
 
 UPDATE VISTA_ACTIVIDADES_ALOJAMIENTO
   SET NOMBRE_ALOJAMIENTO = 'TEST'
   WHERE ID_ALOJAMIENTO = 1;
   
 -- Falta terminar de meter todos los campos de las 2 (?) tablas aqui.  
-INSERT INTO vista_actividades_alojamiento (id_actividad, nombre_alojamiento, nombre_actividad, direccion_social, razon_social, telefono_contacto)
-  VALUES(6, 'nombreAloj.', 'nombreActiv.', 'dirSoc', 'razSoc', 'telefono');
+INSERT INTO vista_actividades_alojamiento (id_actividad, nombre_alojamiento, nombre_actividad, dificultad, capacidad, direccion_social, razon_social, telefono_contacto, descripcion_alojamiento, descripcion_actividad, dia_realizacion, dia_semana, valoracion_alojamiento, fecha_apertura, numero_habitaciones, provincia, localizacion, hora_inicio, hora_fin, nombre_guia)
+  VALUES(11, 'nombreAloj.', 'nombreActiv.', 5, 20, 'dirSoc', 'razSoc', 'telefono', 'descripcion aloha', 'descripcion actividad', '23/11/1993', 'lunes', 5, 'fecha_apertura', 10, 'provincia', 'localizacion', '8:30', '15:00', 'Antonio');
