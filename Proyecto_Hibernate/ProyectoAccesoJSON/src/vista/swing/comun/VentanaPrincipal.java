20c33d02b0600d94d5b4b7a49efc516718867166
Mario <mariocodes1993@gmail.com>
2017-01-27 13:03:15 +0100

    Movido el main a VentanaPrincipal(). Hare ventana de logeo si me sobra tiempo.

--- Proyecto_Hibernate/ProyectoAccesoJSON/src/vista/swing/comun/VentanaPrincipal.java
+++ Proyecto_Hibernate/ProyectoAccesoJSON/src/vista/swing/comun/VentanaPrincipal.java
@@ -5,11 +5,11 @@
  */
 package vista.swing.comun;
 
-import controlador.datos.DBBConexion;
+import posibles.DBBConexion;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import vista.swing.vistasHibernate.VentanaAltaYModifVista;
+import vista.swing.vista.VentanaAltaYModifVista;
 
 /**
  * Ventana Principal del programa. Conduce al resto de Ventanas que haran las operaciones.
@@ -404,6 +404,41 @@ public class VentanaPrincipal extends javax.swing.JFrame  {
         confirmacionVolverVentanaPrincipalGuardarCambiosJSON();
     }//GEN-LAST:event_jMenuItemEscogerModoActionPerformed
        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaPrincipal().setVisible(true);
+            }
+        });
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonAltaAlojamiento;
     private javax.swing.JButton botonAltaHabitacion;
