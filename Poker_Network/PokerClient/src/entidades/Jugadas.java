308b099167ad14b5850483716a9476e4b6381a1b
Mario Codes <mariocodes1993@gmail.com>
2017-02-17 22:53:14 +0100

    Doble pareja implementada y comprobada. Javadoc pendiente hasta ahora creado. A por trio.

--- Poker_Network/PokerClient/src/entidades/Jugadas.java
+++ Poker_Network/PokerClient/src/entidades/Jugadas.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 
 /**
  * Clase estatica para recopilar las comprobaciones de la mano que tiene cada jugador.
+ * Se va recorriendo desde la mas alta, y en cuanto detecta una se devuelve esa.
  * @author Mario Codes Sánchez
  * @since 17/02/2017
  */
@@ -57,22 +58,62 @@ public class Jugadas {
         return carta.toString().substring(2);
     }
     
-//    private static boolean checkDoblePareja(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
-//        int parejas = 0;
-//        int primerValor = getValor(propias.get(0));
-//        if(getValor(propias.get(1)) == primerValor)
-//    }
+    /**
+     * Check para comprobar si tiene dos parejas independientes.
+     * @param propias Cartas propias del jugador.
+     * @param comunes Cartas comunes a todos.
+     * @return True si dispone de doble pareja.
+     */
+    private static boolean checkDoblePareja(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
+        ArrayList<Integer> valores = getValores(propias, comunes);
+        boolean parejaEncontrada = false;
+        int parejaUno = 0;
+        
+        for (int i = 0; i < valores.size() && !parejaEncontrada; i++) { //Buscado de una primera pareja.
+            int valor = valores.get(i);
+            for (int j = 0; j < valores.size() && !parejaEncontrada; j++) {
+                int valor2 = valores.get(j);
+                if(j != i) {
+                    if(valor == valor2) {
+                        parejaUno += valor+valor2;
+                        parejaEncontrada = true;
+                    }
+                }
+            }
+        }
+        
+        if(parejaUno == 0) return false; //No hace falta que siga buscando.
+
+        for (int i = 0; i < valores.size(); i++) { //Segunda pareja, que no sea la primera.
+            int valor = valores.get(i);
+            for (int j = 0; j < valores.size(); j++) {
+                int valor2 = valores.get(j);
+                if(j != i) {
+                    if(valor == valor2 && ((valor+valor2) != parejaUno)) {
+                        Jugadas.valor += (valor+valor2+parejaUno);
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
     
+    /**
+     * Comprobacion de si el jugador dispone de una pareja simple. (2 cartas iguales).
+     * @param propias Cartas propias del jugador.
+     * @param comunes Cartas comunes a todos.
+     * @return True si dispone de pareja.
+     */
     public static boolean checkPareja(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
         ArrayList<Integer> valores = getValores(propias, comunes);
         
         for (int i = 0; i < valores.size(); i++) {
             int valor = valores.get(i);
-            System.out.println("Valor1: " +valor);
             for (int j = 0; j < valores.size(); j++) {
                 int valor2 = valores.get(j);
                 if(j != i) {
-                    System.out.println("Check contra: " +valores.get(j));
                     if(valor == valor2) {
                         Jugadas.valor = valor+valor2;
                         return true;
@@ -84,6 +125,12 @@ public class Jugadas {
         return false;
     }
     
+    /**
+     * Comprobacion de cual es la carta mas alta.
+     * @param propias Cartas propias del jugador.
+     * @param comunes Cartas comunes a todos.
+     * @return True siempre ya que es la minima combinacion posible.
+     */
     private static boolean checkCartaAlta(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
         int maxValor = getValor(propias.get(0));
         if(getValor(propias.get(1)) > maxValor) maxValor = getValor(propias.get(1));
@@ -97,6 +144,12 @@ public class Jugadas {
         return true;
     }
     
+    /**
+     * Mezcla de todas las cartas para chequear combinaciones posibles en una unica ArrayList.
+     * @param propias Cartas propias del Jugador.
+     * @param comunes Cartas comunes a todos.
+     * @return ArrayList conjunta a todos los jugadores.
+     */
     private static ArrayList<Integer> getValores(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
         ArrayList<Integer> valores = new ArrayList<>();
         for(Carta c: propias) valores.add(getValor(c));
@@ -105,10 +158,19 @@ public class Jugadas {
         return valores;
     }
     
+    /**
+     * Comprobacion de la jugada posible que tiene un jugador, dado el conjunto de cartas.
+     * Modifica la String para saber la jugada, y el valor por si hay empate de jugadas.
+     * @param propias Cartas propias. Siempre son 2.
+     * @param comunes Cartas comunes a todos, pueden ser 3, 4 o 5.
+     */
     public static void checkJugada(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
-        if(checkPareja(propias, comunes)) Jugadas.jugada = "Pareja";
+        if(checkDoblePareja(propias, comunes)) Jugadas.jugada = "Doble Pareja";
         else {
-            if(checkCartaAlta(propias, comunes)) Jugadas.jugada = "Carta Alta";
+            if(checkPareja(propias, comunes)) Jugadas.jugada = "Pareja";
+            else {
+                if(checkCartaAlta(propias, comunes)) Jugadas.jugada = "Carta Alta";
+            }
         }
     }
 }
