f3aad44cf8499fef34d8908d3ff7c2ab2ee72a8e
Mario Codes <mariocodes1993@gmail.com>
2017-02-17 22:01:33 +0100

    Antes de mover las comprobaciones de las manos a su clase propia.

---
+++ Poker_Network/PokerClient/src/entidades/Jugadas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ * Clase estatica para recopilar las comprobaciones de la mano que tiene cada jugador.
+ * @author Mario Codes Sánchez
+ * @since 17/02/2017
+ */
+public class Jugadas {
+    
+}
--- Poker_Network/PokerClient/src/entidades/Mano.java
+++ Poker_Network/PokerClient/src/entidades/Mano.java
@@ -6,6 +6,7 @@
 package entidades;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Mano del Jugador, se compone de sus 2 cartas personales y las 3 mas altas de las 5 de la mesa.
@@ -15,6 +16,8 @@ import java.util.ArrayList;
 public class Mano {
     private ArrayList<Carta> cartas_propias = new ArrayList<>();
     private ArrayList<Carta> cartas_mesa = new ArrayList<>();
+    private String jugada;
+    private int valor;
     
     /**
      * Aniadido de una unica carta a la mano personal.
@@ -39,20 +42,97 @@ public class Mano {
             switch(v) {
                 case "A":
                     valor = 14;
+                    break;
                 case "K":
                     valor = 13;
+                    break;
                 case "Q":
                     valor = 12;
+                    break;
                 case "J":
                     valor = 11;
+                    break;
             }
         }
         
         return valor;
     }
     
+    /**
+     * Obtencion del palo de la carta para comprobar escaleras, fulls y demas.
+     * @param carta Carta a obtener el palo.
+     * @return Palo de la carta introducida.
+     */
+    private String getPalo(Carta carta) {
+        return carta.toString().substring(2);
+    }
+    
+    /**
+     * Para los casos en los que las 5 cartas de la mesa hagan escalera.
+     * @param valoresComunes 5 cartas de la mesa transformadas en su valor int.
+     * @return True si las 5 hacen escalera.
+     */
+    private boolean checkEscaleraComunes(ArrayList<Integer> valoresComunes) {
+        if(valoresComunes.size() == 5) {
+            int firstNum = valoresComunes.get(0);
+            for (int i = 1; i < valoresComunes.size(); i++) {
+                if(valoresComunes.get(i) != firstNum+1) return false;
+                else firstNum++;
+            }
+            return true;
+        } else return false;
+    }
+    
+    private boolean checkEscalera(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
+        ArrayList<Carta> tmp = new ArrayList<>();
+        tmp.addAll(propias);
+        
+        ArrayList<Integer> valoresPropias = new ArrayList<>();
+        for(Carta c: propias) valoresPropias.add(getValor(c));
+        valoresPropias.sort(Comparator.naturalOrder());
+//        for(int i : valoresPropias) System.out.println(i);
+        
+        ArrayList<Integer> valoresComunes = new ArrayList<>();
+        for(Carta c: comunes) valoresComunes.add(getValor(c));
+        valoresComunes.sort(Comparator.naturalOrder());
+//        for(int i : valoresComunes) System.out.println(i);
+        
+        if(checkEscaleraComunes(valoresComunes)) return true;
+        
+        int primerValor = getValor(comunes.get(0));
+        for (int i = 0; i < comunes.size()+2; i++) {
+            
+        }
+        
+        return false;
+    }
+    
+    private boolean checkPareja(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
+        int primerValor = getValor(propias.get(0));
+        if(getValor(propias.get(1)) == primerValor) return true;
+        else {
+            for(Carta c: comunes) {
+                if(getValor(c) == primerValor) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean cartaAlta(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
+        int primerValor = getValor(propias.get(0));
+        if(getValor(propias.get(1)) > primerValor) return true;
+        else {
+            for(Carta c: comunes) {
+                if(getValor(c) >= primerValor) return true;
+            }
+        }
+        
+        return true;
+    }
+    
     private ArrayList<Carta> compararCartas(ArrayList<Carta> propias, ArrayList<Carta> comunes) {
-        ArrayList<Carta> mejorCombinacion = new ArrayList<>();
+        checkEscalera(propias, comunes);
         
         return null;
     }
