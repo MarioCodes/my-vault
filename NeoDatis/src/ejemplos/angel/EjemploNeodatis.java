33dd0ae89e22c7fc5b1507730ce13eb2865527a1
Mario Codes <mariocodes1993@gmail.com>
2017-02-21 23:27:29 +0100

    Conseguido get e insert en NeoDatis, seguir manipulando los ejemplos de Angel

--- NeoDatis/src/ejemplos/angel/EjemploNeodatis.java
+++ NeoDatis/src/ejemplos/angel/EjemploNeodatis.java
@@ -1,7 +1,46 @@
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
 import org.neodatis.odb.Objects;
-//Clase Jugadores
+
+public class EjemploNeodatis {	
+    public static void main(String[] args) {
+        getDatos();
+   }
+    
+    public static void insert() {
+        ODB odb = ODBFactory.open("neodatis.test");// Abrir BD
+        
+        Jugadores j1 = new Jugadores("Maria", "voleibol", "Madrid", 14); // Crear instancias para almacenar en BD
+        Jugadores j2 = new Jugadores("Miguel", "tenis", "Madrid", 15);
+        Jugadores j3 = new Jugadores("Mario", "baloncesto", "Guadalajara", 15);
+        Jugadores j4 = new Jugadores("Alicia", "tenis", "Madrid", 14);
+        
+        odb.store(j1); 
+        odb.store(j2);
+        odb.store(j3);
+        odb.store(j4);
+
+        odb.close();
+    }
+    
+    public static void getDatos() {
+        ODB odb = ODBFactory.open("neodatis.test");// Abrir BD
+        
+        Objects<Jugadores> objects = odb.getObjects(Jugadores.class);
+        System.out.printf("%d Jugadores: %n", objects.size());
+
+        int i = 1;
+        // visualizar los objetos		
+        while(objects.hasNext()){
+           Jugadores jug = objects.next();
+           System.out.printf("%d: %s, %s, %s %n",
+                     i++, jug.getNombre(), jug.getDeporte(),
+                     jug.getCiudad(), jug.getEdad());      
+        }		
+        odb.close(); // Cerrar BD				
+    }
+}
+
 class Jugadores {
     private String nombre;
     private String deporte;
@@ -25,38 +64,4 @@ class Jugadores {
     public String getCiudad () {return ciudad;}
     public void setEdad(int edad) {this.edad = edad;}
     public int getEdad() {return edad;}
-}
-// 
-public class EjemploNeodatis {	
-    public static void main(String[] args) {
-
-      // Crear instancias para almacenar en BD
-      Jugadores j1 = new Jugadores("Maria", "voleibol", "Madrid", 14);
-      Jugadores j2 = new Jugadores("Miguel", "tenis", "Madrid", 15);
-      Jugadores j3 = new Jugadores
-                    ("Mario", "baloncesto", "Guadalajara", 15);
-      Jugadores j4 = new Jugadores("Alicia", "tenis", "Madrid", 14);
-
-      ODB odb = ODBFactory.open("neodatis.test");// Abrir BD
-
-      // Almacenamos objetos
-      odb.store(j1); 
-      odb.store(j2);
-      odb.store(j3);
-      odb.store(j4);
-				
-      //recuperamos todos los objetos
-      Objects<Jugadores> objects = odb.getObjects(Jugadores.class);
-      System.out.printf("%d Jugadores: %n", objects.size());
-
-      int i = 1;
-      // visualizar los objetos		
-      while(objects.hasNext()){
-         Jugadores jug = objects.next();
-         System.out.printf("%d: %s, %s, %s %n",
-      		   i++, jug.getNombre(), jug.getDeporte(),
-      		   jug.getCiudad(), jug.getEdad());      
-      }		
-     odb.close(); // Cerrar BD				
-   }
 }
