3703c5a2b3458c4da7268c6940ce8bd976e512c5
Mario Codes <mariocodes1993@gmail.com>
2018-01-14 14:39:33 +0100

    lambdas: second example

--- Java8/src/test/java/lambdas/Collections.java
+++ Java8/src/test/java/lambdas/Collections.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public class Collections {
 	final List<String> friends = Arrays.asList("Moha", "Carlos", "Carlos Arturo", "Alejandro",
 			"Maria", "Ramon");
 
+	// Example 1
 	@Test
 	@Ignore("Do not use this, use method reference.")
 	public void givenFriendsWhenIterateThenPrintAllOldWay() {
@@ -17,8 +19,27 @@ public class Collections {
 	}
 
 	@Test
+	@Ignore
 	public void givenFriendsWhenIterateThenPrintWithMethodReference() throws Exception {
 		friends.forEach(System.out::println);
 	}
 
+	// Example 2
+	@Test
+	@Ignore("Old way")
+	public void givenFriendsWhenUpperCaseOldWayThenPrint() throws Exception {
+		ArrayList<String> upperCase = new ArrayList<>();
+		for (String name : friends)
+			upperCase.add(name.toUpperCase());
+
+		for (String name : upperCase)
+			System.out.println(name);
+	}
+
+	@Test
+	public void givenFriendsWhenUpperCaseThenPrint() throws Exception {
+		friends.stream()
+				.map(String::toUpperCase)
+				.forEach(System.out::println);
+	}
 }
