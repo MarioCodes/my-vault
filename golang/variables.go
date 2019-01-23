aa413540cf488e1ed63630f77cc4f5163cd4ca5a
Mario Codes Sánchez <m.sanchez@neusta.de>
2019-01-23 09:23:36 +0100

    golang: variables

--- golang/variables.go
+++ golang/variables.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var age int // variable declaration
-	var footSize = 42
-	fmt.Println("my age is:", age, "and my foot size: ", footSize)
+	var age int                                                    // variable declaration with type
+	var footSize = 42                                              // type inference
+	fmt.Println("my age is:", age, "and my foot size: ", footSize) // the space btw. variables is automatically given
 }
