9e9090dbdcec6bf9dd643f2617837aae4b4a52d2
Mario Codes <mariocodes1993@gmail.com>
2019-06-19 13:38:49 +0200

    kotlin: deleted template placeholders

--- kotlin-react/kotlin-backend/src/main/java/es/msanchez/fullstack/kotlin/dao/HobbyDao.kt
+++
@@ -1,7 +0,0 @@
-package es.msanchez.fullstack.kotlin.dao
-
-import es.msanchez.fullstack.kotlin.entity.Hobby
-import org.springframework.stereotype.Repository
-
-@Repository
-interface HobbyDao : RawDao<Hobby>
