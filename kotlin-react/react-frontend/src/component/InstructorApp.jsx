b51ea7414ad571525968a24cf4f7d9b0494c7d8a
Mario Codes <mariocodes1993@gmail.com>
2019-06-19 15:01:05 +0200

    react: writing API consumer

---
+++ kotlin-react/react-frontend/src/component/InstructorApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class InstructorApp extends Component {
+  render() {
+    return (
+      <h1>Instructor Application</h1>
+    )
+  }
+}
+
+export default InstructorApp
