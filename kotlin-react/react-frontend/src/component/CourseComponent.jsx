32b6e1d1d19d3c6326c4ee127a3ec7bc5383990f
Mario Codes <mariocodes1993@gmail.com>
2019-08-14 16:32:43 +0200

    kotlin-react: update courses

---
+++ kotlin-react/react-frontend/src/component/CourseComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CourseComponent extends React.Component {
+    render() {
+        return (
+            <h1>Course Details</h1>
+        )
+    }
+}
+
+export default CourseComponent
